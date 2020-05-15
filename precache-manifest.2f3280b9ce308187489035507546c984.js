babf586726cd6291aca7eb01ba841a1473e9aaf7
SunHarry198 <50999881+SunHarry198@users.noreply.github.com>
2020-05-15 16:11:48 +0700

    make it better

---
+++ precache-manifest.2f3280b9ce308187489035507546c984.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af4e9c5741ba233c4f63d4ab6c86803",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b5fb41e9741a9b11463",
+    "url": "/static/css/main.56315c9c.chunk.css"
+  },
+  {
+    "revision": "21a999e304b697dbf3c9",
+    "url": "/static/js/2.718e9500.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.718e9500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b5fb41e9741a9b11463",
+    "url": "/static/js/main.e66e39c2.chunk.js"
+  },
+  {
+    "revision": "aeb31f32d2765057b623",
+    "url": "/static/js/runtime-main.53b04572.js"
+  },
+  {
+    "revision": "1b5b8f77e93f0dafc41b0bc65797c252",
+    "url": "/static/media/OscineTrial-Light.1b5b8f77.eot"
+  },
+  {
+    "revision": "3ff0f52c6ecf8fdf19ba531b42f698ed",
+    "url": "/static/media/OscineTrial-Light.3ff0f52c.ttf"
+  },
+  {
+    "revision": "6d0b2b0efe71b1da6114edaed2908c6b",
+    "url": "/static/media/OscineTrial-Light.6d0b2b0e.svg"
+  },
+  {
+    "revision": "cc57cfee72a5b6499a66efab8083aadc",
+    "url": "/static/media/OscineTrial-Light.cc57cfee.woff"
+  }
+]);
